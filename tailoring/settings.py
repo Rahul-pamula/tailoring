2a2f42f6d04a5e13b83265918f602a6c9f7f04d3
pamula rahul <pamula@pamula-rahuls-MacBook-Air.local>
2025-02-27 22:52:12 +0530

    Trigger redeployment

--- tailoring/settings.py
+++ tailoring/settings.py
@@ -71,6 +71,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+print("Deployment triggered")
 
 # ✅ Cloudinary Configuration (Fix indentation)
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
