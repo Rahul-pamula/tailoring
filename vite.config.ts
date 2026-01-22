d71283b7dc54b838d02b9ca534be7eb21cf83eda
rahul-pamula <rahulpamula123@gmail.com>
2026-01-22 18:23:55 +0530

    Remove base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tailoring/',  // GitHub Pages base path
   server: {
     host: "::",
     port: 8080,
